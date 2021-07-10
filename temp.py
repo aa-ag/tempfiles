79000a8a9f7f3a6412b57db2aa29f264f186d7ef
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-09 20:12:41 -0500

    added temp.py and its base code structure

---
+++ temp.py
@@ -0,0 +1,11 @@
+############------------ IMPORTS ------------############
+import tempfile
+
+############------------ CREATE SELFDESTRUCTING FILE ------------############
+def self_destructing_file():
+    pass
+
+
+############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    self_destructing_file()
